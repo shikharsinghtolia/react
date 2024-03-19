a3b71415283d1a0d8eb3e7b8f5c581e6134afa8d
Shikhar <Shikharsama@gmail.com>
2024-03-19 12:08:23 +0530

    abc

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  let name = "Shikhar Singh Tolia";
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About </li>
+      <li>Contact</li>
+      <h1>Hello {name}</h1>
+    </nav>
+    </>
+    
+  );
+}
+
+export default App;
