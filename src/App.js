cfc7aec2a56040d087c31cb14a7dd3e68a906061
Shikhar <Shikharsama@gmail.com>
2024-03-21 19:51:42 +0530

    re-set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import NavBar from "./components/NavBar";
-import TextForm from "./components/TextForm";
 
 function App() {
   
   return (
     <> 
-      <NavBar title="TextUtils"/>
-      <div className="container my-3">
-        <TextForm textArea="Enter text to analyze"/>
-      </div>
+      <h1>hey</h1>
+      
       
     </>
 
